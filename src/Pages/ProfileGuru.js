f6291932731d7a6cbc39a269571fc120735bd6f1
kobar <kobar@alphatech.id>
2018-11-29 12:29:42 +0700

    add pages profile

---
+++ src/Pages/ProfileGuru.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Profile extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Ini profile</h1>
+            </div>
+        )
+    }
+}
+
+export default Profile
